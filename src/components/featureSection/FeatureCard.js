a76896d4dcc13cb1e8b1d68b66673166ea0a6cf9
shiv-anya <sg90883@gmail.com>
2023-07-03 15:45:28 +0530

    added alt attribute

--- src/components/featureSection/FeatureCard.js
+++ src/components/featureSection/FeatureCard.js
@@ -3,12 +3,12 @@ import Arrow from "../../assets/Vector 3.svg"
 
 const FeatureCard = (props) => {
     return <div className="flex flex-col justify-between bg-white px-10 pt-10 pb-5 w-64 h-80 rounded-2xl">
-        <div><img  src={props.img} alt={props.title}/></div>
+        <div><img src={props.img} alt={props.title}/></div>
         <div className="text-left">
             <h2 className="text-xl">{props.title}</h2>
             <p className="text-sm text-gray-400">{props.content}</p>
         </div>
-        <button className="flex items-center text-purple-800">See Explained <img src={Arrow} className="ml-2"/></button>
+        <button className="flex items-center text-purple-800">See Explained <img alt="" src={Arrow} className="ml-2"/></button>
     </div>
 }
 
