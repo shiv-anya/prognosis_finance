0104908412e5199d104c169c1c07e57ca118e7a2
shiv-anya <sg90883@gmail.com>
2023-07-03 12:19:45 +0530

    added required colors and backgroundColor

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      textColor: {
+        primaryGray: "#C4C4C4"
+      },
+      backgroundColor: {
+        primaryGreen: "#26B645",
+        greenDark: "#0FAE96",
+      }
+    },
+  },
+  plugins: [],
+}
