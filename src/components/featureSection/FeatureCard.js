7d75e75eeebfa883230d48f50406e859396bbcd1
shiv-anya <sg90883@gmail.com>
2023-07-03 15:34:20 +0530

    added alt

--- src/components/featureSection/FeatureCard.js
+++ src/components/featureSection/FeatureCard.js
@@ -3,7 +3,7 @@ import Arrow from "../../assets/Vector 3.svg"
 
 const FeatureCard = (props) => {
     return <div className="flex flex-col justify-between bg-white px-10 pt-10 pb-5 w-64 h-80 rounded-2xl">
-        <div><img src={props.img} alt={props.title}/></div>
+        <div><img  src={props.img} alt={props.title}/></div>
         <div className="text-left">
             <h2 className="text-xl">{props.title}</h2>
             <p className="text-sm text-gray-400">{props.content}</p>
