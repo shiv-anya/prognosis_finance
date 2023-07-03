623ee814f9fd007595d233e32fde3d2e517a4d1f
shiv-anya <sg90883@gmail.com>
2023-07-03 19:04:50 +0530

    added pop up

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import Popup from "./Popup";
 
 const Banner = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const earlyAccessListener = () => {
+    setIsClicked(!isClicked);
+  }
   return (
+    <Fragment>
+    {isClicked && <Popup/>}
     <section className="h-96 text-white flex justify-center align-center">
       <div className="text-primaryGray text-center">
         <h2 className="text-4xl pt-28 pb-3">Prognosis Finance</h2>
@@ -11,9 +18,10 @@ const Banner = () => {
           <p className="pb-3 text-lg">analysis based about the best practices... you are one
           click away
         </p>
-        <button className="rounded-xl text-base mt-8 mb-10 text-white bg-primaryGreen px-6 py-4">Early Access</button>
+        <button onClick={earlyAccessListener} className="rounded-xl text-base mt-8 mb-10 text-white bg-primaryGreen px-6 py-4">Early Access</button>
       </div>
     </section>
+    </Fragment>
   );
 };
 
---
+++ src/components/Popup.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Cross from "../assets/cross.svg"
+
+const Popup = () => {
+    return (
+        <div className="fixed flex justify-center w-full h-screen inset-0 top-10">
+        <div className="bg-white rounded-lg h-popup w-popup px-10 pb-10">
+            <div className="flex justify-end py-2"><div className="bg-gray-300 p-2 rounded-full"><img alt="cross" src={Cross}/></div></div>
+            <div>
+                <div className="w-82 h-32 bg-gray-300 rounded-lg"></div>
+                <div className="text-center my-2">
+                    <h2>Subscribe to our Early access</h2>
+                    <p>Join thousands getting emails in their inbox.</p>
+                </div>
+                <form className="flex flex-col">
+                    <input type="text" placeholder="ADA address" className="rounded-lg py-3 px-5 border border-solid border-gray-300 mb-2"/>
+                    <input type="text" placeholder="Twitter username" className="rounded-lg py-3 px-5 border border-solid border-gray-300 mb-2" />
+                    <input type="text" placeholder="Discord Username" className="rounded-lg py-3 px-5 border border-solid border-gray-300 mb-2"/>
+                    <button className="text-white rounded-lg py-3 px-5 bg-blue-300 mb-2 bg-blue-700">Tweet About Us!</button>
+                    <button className="text-white bg-gray-500 rounded-lg py-3 px-5 bg-blue-300 mb-2">Register here!</button>
+                </form>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Popup;
