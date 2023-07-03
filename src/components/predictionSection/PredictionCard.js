f46b341e5964ceef5004bf40e553b87b2b4239d8
Prashant varma <prashantvarma5083@gmail.com>
2023-07-03 19:33:14 +0530

    Animation added

--- src/components/predictionSection/PredictionCard.js
+++ src/components/predictionSection/PredictionCard.js
@@ -1,16 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const PredictionCard = () => {
   return (
-    <div className="flex flex-col justify-evenly bg-white h-48 rounded-xl w-64 px-2 pt-2">
-      <div><img src="https://source.unsplash.com/random/?person" alt="person" className="h-14"/></div>
-      <div>
-        <p className="text-xs font-bold mb-2">india will record highest temperatues in 2023 </p>
-        <div className="flex">
-            <div className="flex text-xs p-2 font-bold justify-between w-3/5 bg-red-300 mr-2"><p>Yes</p><p className="text-red-500">4.5 $</p></div>
-            <div className="flex text-xs p-2 font-bold justify-between w-2/5 bg-green-300"><p>No</p><p className="text-green-500">3.5 $</p></div>
+    <div className="Scroll-remove" style={{ overflowX: 'hidden' }}>
+      <motion.div
+        className="flex flex-col justify-evenly bg-white h-48 rounded-xl w-64 px-2 pt-2"
+        style={{ overflowX: 'hidden' }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div>
+          <img src="https://source.unsplash.com/random/?person" alt="person" className="h-14" />
         </div>
-      </div>
+        <div>
+          <p className="text-xs font-bold mb-2">India will record the highest temperatures in 2023 </p>
+          <div className="flex">
+            <div className="flex text-xs p-2 font-bold justify-between w-3/5 bg-red-300 mr-2">
+              <p>Yes</p>
+              <p className="text-red-500">4.5 $</p>
+            </div>
+            <div className="flex text-xs p-2 font-bold justify-between w-2/5 bg-green-300">
+              <p>No</p>
+              <p className="text-green-500">3.5 $</p>
+            </div>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/trendingSection/TrendingCard.js
+++ src/components/trendingSection/TrendingCard.js
@@ -1,14 +1,19 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const TrendingCard = () => {
     return (
-        <div className="flex flex-col justify-end h-48 w-64 rounded-lg bg-white font-bold text-base">
+        <motion.div className="flex flex-col justify-end h-48 w-64 rounded-lg bg-white font-bold text-base"
+        style={{ overflowX: 'hidden' }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        >
             <p className="p-2">India will win world cup in 2023</p>
             <div className="flex border-t-purple-600 border-t border-solid">
                 <div className="flex-1 p-2 flex justify-center items-center"><p>Yes</p><p className="text-green-600 ml-4">16$</p></div>
                 <div className="flex-1 p-2 flex justify-center items-center border-l border-l-purple-600 border-solid"><p>No</p><p className="text-red-600 ml-4">28$</p></div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
